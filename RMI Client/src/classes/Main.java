28a67a20db90e0bd5885499d452b928e024711a0
? <alberto.garcia.2016@upb.edu.co>
2020-02-21 15:20:16 -0500

    Update Main.java

--- RMI Client/src/classes/Main.java
+++ RMI Client/src/classes/Main.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,12 +12,22 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+             Scanner myObj = new Scanner(System.in);
 		try {
 			iRMI service =  (iRMI) Naming.lookup("rmi://127.0.0.1:1802/service");			
                         System.out.println("conecto");	
-                        System.out.println("elija tipo de usuario. ");
+                        System.out.println("elija tipo de usuario (0/1) ");
+                        String tipoUsuario = myObj.nextLine();
                         
-                        
+                        System.out.println("elija nombre de usuario (0/1) ");
+                        String nombreUsuario = myObj.nextLine();
+                        String opcion="-1";
+                        do{
+                            System.out.println("Escoja su opción");
+                            System.out.println("1. Crear \n2. Leer\n3. Actualizar\n4. Eliminar\n0. Salir");
+                            opcion =myObj.nextLine();
+                            
+                        }while(opcion!="0");
 		} catch (MalformedURLException | RemoteException | NotBoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
